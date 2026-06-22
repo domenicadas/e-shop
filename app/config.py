9227c1774a939a082234f6cd59e9c918cfe188ef
Domenica Sarango <dsarangoaest.ups.edu.ec>
2026-06-22 16:56:21 -0500

    Correcion de db

--- app/config.py
+++ app/config.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-  SECRET_KEY= os.getenv('SECRET_KEY','dev-secret')
-  #Configuracion de la DB
-  SQLALCHEMY_DATABASE_URI={
-    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-  }
-
-  SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
+    # Configuración de la BD
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
